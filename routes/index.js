1746900e886dba9b75b9c5c5c0285c50d9a24331
Joe Smith <yasumoto7@gmail.com>
2012-07-09 00:28:45 -0700

    Mess around with the walk module, print out contents of dir to console.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
  * GET home page.
  */
 
+var media = require('../lib/media.js');
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
+  media.findMediaFiles('/usr/local/Media');
 };
